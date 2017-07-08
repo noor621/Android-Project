79ecc93ab8cb97c11fa16ed3a90062d3d5a895ed
Mahnoor Saeed <mahnoorsaeed025@gmail.com>
2017-07-08 05:34:23 +0500

    This is first commit

---
+++ RestfulServiceForAndroidProject/RestfulServiceForAndroidProject/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestfulServiceForAndroidProject.Models;
+
+namespace RestfulServiceForAndroidProject.Controllers
+{
+    
+    public class StudentsController : ApiController
+    {
+        private MC_ProjectAssignerEntities db = new MC_ProjectAssignerEntities();
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            var list = db.Students.ToList(); 
+      
+            return list;
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertStudent(Student student)
+        {
+            
+            db.Students.Add(student);
+            db.SaveChangesAsync();
+           
+            return Ok(student.SEmailID);
+        }
+    }
+}
---
+++ RestfulServiceForAndroidProject/RestfulServiceForAndroidProject/Controllers/checkController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestfulServiceForAndroidProject.Models;
+
+namespace RestfulServiceForAndroidProject.Controllers
+{
+    public class checkController : Controller
+    {
+        private MC_ProjectAssignerEntities db = new MC_ProjectAssignerEntities();
+
+        // GET: /check/
+        public ActionResult Index()
+        {
+            return View(db.Groups.ToList());
+        }
+
+        // GET: /check/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // GET: /check/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /check/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="GID,GName")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Groups.Add(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(group);
+        }
+
+        // GET: /check/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: /check/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="GID,GName")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(group);
+        }
+
+        // GET: /check/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: /check/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Group group = db.Groups.Find(id);
+            db.Groups.Remove(group);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
